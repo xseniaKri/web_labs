46d8f7df6213c54452a6ba9e8e2ca626c78f0598
xseniaKri <kseniaenta@mail.ru>
2026-03-25 18:14:54 +0300

    feat: сделала лр 2

--- app/app.py
+++ app/app.py
@@ -1,52 +1,93 @@
-import random
-from flask import Flask, render_template
-from faker import Faker
-
-fake = Faker()
+from flask import Flask, render_template, request, make_response
+import re
 
 app = Flask(__name__)
-application = app
 
-images_ids = ['7d4e9175-95ea-4c5f-8be5-92a6b708bb3c',
-              '2d2ab7df-cdbc-48a8-a936-35bba702def5',
-              '6e12f3de-d5fd-4ebb-855b-8cbc485278b7',
-              'afc2cfe7-5cac-4b80-9b9a-d5c65ef0c728',
-              'cab5b7f2-774e-4884-a200-0c0180fa777f']
 
-def generate_comments(replies=True):
-    comments = []
-    for i in range(random.randint(1, 3)):
-        comment = { 'author': fake.name(), 'text': fake.text() }
-        if replies:
-            comment['replies'] = generate_comments(replies=False)
-        comments.append(comment)
-    return comments
-
-def generate_post(i):
-    return {
-        'title': 'Заголовок поста',
-        'text': fake.paragraph(nb_sentences=100),
-        'author': fake.name(),
-        'date': fake.date_time_between(start_date='-2y', end_date='now'),
-        'image_id': f'{images_ids[i]}.jpg',
-        'comments': generate_comments()
-    }
-
-posts_list = sorted([generate_post(i) for i in range(5)], key=lambda p: p['date'], reverse=True)
-
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/posts')
-def posts():
-    return render_template('posts.html', title='Посты', posts=posts_list)
 
-@app.route('/posts/<int:index>')
-def post(index):
-    p = posts_list[index]
-    return render_template('post.html', title=p['title'], post=p)
+@app.route("/request-info", methods=["GET", "POST"])
+def request_info():
 
-@app.route('/about')
-def about():
-    return render_template('about.html', title='Об авторе')
+    url_params = request.args
+    headers = request.headers
+    cookies = request.cookies
+    form_data = request.form
+
+    response = make_response(
+        render_template(
+            "request_info.html",
+            url_params=url_params,
+            headers=headers,
+            cookies=cookies,
+            form_data=form_data
+        )
+    )
+
+    response.set_cookie("example_cookie", "HelloCookie")
+
+    return response
+
+
+@app.route("/phone", methods=["GET", "POST"])
+def phone():
+
+    error = None
+    phone_formatted = None
+    phone_input = ""
+
+    if request.method == "POST":
+
+        phone_input = request.form.get("phone")
+
+        if not re.fullmatch(r"[0-9+\-\s().]+", phone_input):
+            error = (
+                "Недопустимый ввод. "
+                "В номере телефона встречаются "
+                "недопустимые символы."
+            )
+
+        else:
+            digits = re.sub(r"\D", "", phone_input)
+
+            if phone_input.startswith("+7") or phone_input.startswith("8"):
+
+                if len(digits) != 11:
+                    error = (
+                        "Недопустимый ввод. "
+                        "Неверное количество цифр."
+                    )
+
+            else:
+
+                if len(digits) != 10:
+                    error = (
+                        "Недопустимый ввод. "
+                        "Неверное количество цифр."
+                    )
+
+            if not error:
+
+                if len(digits) == 10:
+                    digits = "8" + digits
+
+                phone_formatted = (
+                    f"8-{digits[1:4]}-"
+                    f"{digits[4:7]}-"
+                    f"{digits[7:9]}-"
+                    f"{digits[9:11]}"
+                )
+
+    return render_template(
+        "phone.html",
+        error=error,
+        phone_formatted=phone_formatted,
+        phone_input=phone_input
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
