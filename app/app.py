23545e806c569d4249d0172177c811a1a74c7674
xseniaKri <kseniaenta@mail.ru>
2026-04-27 15:06:49 +0300

    fix: поправила замечания

--- app/app.py
+++ app/app.py
@@ -15,7 +15,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 login_manager.login_message = "Для доступа необходимо пройти аутентификацию"
-
+login_manager.login_message_category = "WARNING"
 
 class User(UserMixin):
     def __init__(self, id, username, password):
